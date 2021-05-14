8843a8c3da105a50cae420beb0107ecae78b0b57
Galdo <matheus.kho@gmail.com>
2021-05-14 20:14:46 -0300

    adicionando duplicação do modulo de boletos

--- src/views/Boletos/Boletos.js
+++ src/views/Boletos/Boletos.js
@@ -1,6 +1,153 @@
-import './Boletos.scss';
+import { useEffect, useState } from 'react';
+import { Link, Redirect, useHistory } from 'react-router-dom';
+import BackBtn from '../../components/BackBtn/BackBtn';
+import OptionsBtn from '../../components/OptionsBtn/OptionsBtn';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import usePermissao from '../../Hooks/usePermissao';
+import api from '../../Service/api';
+
+
+function downloadFile(e, documento) {
+    e.preventDefault()
+    api(false, 'blob').get(`download-file?file=${documento.id}&module=documento`).then(response => {
+        console.log(response);
+
+        let filename = 'file.txt'
+        try {
+            filename = response.headers['content-disposition'].split(';')[1].split('=')[1]
+        } catch (error) {
+
+        }
+
+        let url = URL.createObjectURL(new Blob([response.data]));
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+        URL.revokeObjectURL(url)
+    })
+}
 
 export default function Boletos(props) {
-    
-    return 'Modulo Boletos'
+
+    const [documentosOriginal, setDocumentosOriginal] = useState([])
+    const [documentos, setDocumentos] = useState([])
+    const [hasLoaded, setHasLoaded] = useState(false)
+
+    const history = useHistory();
+    const { permissao } = usePermissao('documentos')
+
+
+    useEffect(() => {
+        let mounted = true;
+
+        if (!hasLoaded && documentosOriginal.length === 0) {
+            api().get('documentos').then(response => {
+                if (mounted) {
+                    setDocumentosOriginal(response.data)
+                    setDocumentos(response.data)
+                    setHasLoaded(true)
+                }
+            })
+        }
+
+        return () => {
+            mounted = false
+        }
+    }, [documentos, hasLoaded, documentosOriginal])
+
+
+    function filter(e) {
+        let value = e.target.value.toLowerCase()
+
+        if (value === '') {
+            setDocumentos(documentosOriginal);
+            return
+        }
+
+        let filtered = documentosOriginal.filter(documento =>
+            (documento.nome.toLowerCase().indexOf(value) >= 0) ||
+            (documento.nome_original.toLowerCase().indexOf(value) >= 0)
+        )
+
+        setDocumentos(filtered);
+    }
+
+
+    function getItenOptions(item, moduloName, reload) {
+
+        let options = []
+
+        if (permissao.editar) options.push({ name: 'Editar', f: () => history.push(`/${moduloName}/cadastro/${item.id}`) })
+
+        if (item.deleted_at) {
+            options.push({ name: 'Ativar', f: () => api().put(`${moduloName}/${item.id}`, { ativar: true }).then(response => reload(false)) })
+        } else {
+            options.push({ name: 'Desativar', f: () => api().put(`${moduloName}/${item.id}`, { ativar: false }).then(response => reload(false)) })
+        }
+
+        if (permissao.excluir && item.deleted_at) {
+            options.push({ name: 'Excluir', f: () => api().delete(`${moduloName}/${item.id}`).then(response => reload(false)) })
+        }
+
+        return options
+    }
+
+
+    return <div className='module-wrapper'>
+
+        <BackBtn />
+        {permissao.modulo && !permissao.acessar && <Redirect to='/nao-permitido' />}
+
+        <h1>Documentos</h1>
+
+        <div className='top-module-bar'>
+            <SearchBar filter={filter} />
+
+            {permissao.criar && <Link to='/documentos/cadastro/' className='btn-primary'>
+                + Adicionar
+            </Link>}
+        </div>
+
+
+        <div className='list-item-container'>
+
+            {documentos.map((documento, id) => {
+
+                let options = getItenOptions(documento, 'documentos', setHasLoaded)
+
+                return <div key={id} className='list-item-card'>
+                    <Link to={`/documentos/${documento.id}`}>
+
+                        <span className='file-icon'>
+                            {documento.extensao}
+                        </span>
+                    </Link>
+
+                    <div className='list-item-card-content'>
+                        {permissao.visualizar ?
+                            <Link to={`/documentos/${documento.id}`}>
+                                <h1>{documento.nome}</h1>
+                                <p>Status: {documento.deleted_at ? 'Desativado' : 'Ativado'}</p>
+
+                            </Link>
+                            :
+                            <>
+                                <h1>{documento.nome}</h1>
+                                <p>Status: {documento.deleted_at ? 'Desativado' : 'Ativado'}</p>
+                            </>
+                        }
+                    </div>
+
+                    <button onClick={e => downloadFile(e, documento)} className='btn-primary small-btn'>
+                        Download
+                    </button>
+
+                    {permissao.gerenciar && <OptionsBtn options={options} />}
+                </div>
+            })}
+        </div>
+
+
+    </div>
 }
---
+++ src/views/Boletos/Cadastro/Cadastro.js
@@ -0,0 +1,155 @@
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+import { Redirect, useHistory, useParams } from 'react-router';
+import BackBtn from '../../../components/BackBtn/BackBtn';
+import usePermissao from '../../../Hooks/usePermissao';
+import FormInput from '../../../libs/FormInput/FormInput';
+import api from '../../../Service/api';
+
+export default function Cadastro(props) {
+
+    const [nome_arquivo, setNomeArquivo] = useState({ valid: false, errorMessage: "", value: "" })
+    const [publico, setPublico] = useState({ valid: true, errorMessage: "", value: true })
+    const [arquivos, setArquivos] = useState({ valid: false, errorMessage: "", value: {} })
+    const [data_expiracao, setDataExpiracao] = useState({ valid: true, errorMessage: "", value: "" })
+    const [stepTrigered, setStepTrigered] = useState(0)
+    const [hasLoaded, setHasLoaded] = useState(false)
+    const [today] = useState(moment().format('YYYY-MM-DD'))
+
+
+    const history = useHistory();
+    const { permissao } = usePermissao('documentos')
+    let { id } = useParams();
+
+    useEffect(() => {
+        let mounted = true
+
+        if (id && !hasLoaded) {
+            api().get(`documentos/${id}`).then(response => {
+
+                if (mounted) {
+                    setHasLoaded(true)
+                    setNomeArquivo({ valid: false, errorMessage: "", value: response.data.nome })
+                    setPublico({ valid: false, errorMessage: "", value: response.data.is_public })
+                    setDataExpiracao({ valid: false, errorMessage: "", value: response.data.data_expiracao || "" })
+                    setStepTrigered(stepTrigered + 1)
+                }
+            })
+        }
+
+        return () => {
+            mounted = false
+        }
+    }, [stepTrigered, hasLoaded, id])
+
+
+    function submit(e) {
+        e.preventDefault()
+        setStepTrigered(stepTrigered + 1)
+
+        let fields = { publico, arquivos, data_expiracao, nome_arquivo }
+        let valid = true
+        let formData = {}
+
+
+        if (id){
+            delete fields.arquivos
+        
+            Object.keys(fields).forEach(fieldName => {
+                if (!fields[fieldName].valid) valid = false
+    
+                formData[fieldName] = fields[fieldName].value
+            })
+
+        }else{
+            formData = new FormData();
+
+            Object.keys(fields).forEach(fieldName => {
+                if (!fields[fieldName].valid) valid = false
+    
+                if (fieldName === 'arquivos') {
+                    if ('acceptedFiles' in fields[fieldName].value) {
+                        fields[fieldName].value.acceptedFiles.map(file => formData.append(`${fieldName}[]`, file))
+                    }
+                } else {
+                    formData.append(fieldName, fields[fieldName].value);
+                }
+            })
+        }
+
+
+        console.log(formData);
+
+        if (valid) {
+            setStepTrigered(0)
+
+            if (id) {
+                api().patch(`documentos/${id}`, formData).then(response => history.push('/documentos'))
+            } else {
+                api().post('documentos', formData).then(response => history.push('/documentos'))
+            }
+        }
+    }
+
+    return <div className='form-wrapper'>
+
+        {permissao.modulo && (!permissao.acessar || !permissao.criar) && <Redirect to='/nao-permitido' />}
+        {permissao.modulo && id && (!permissao.acessar || !permissao.editar) && <Redirect to='/nao-permitido' />}
+
+        <BackBtn to='/documentos' />
+        <h1>{typeof id === 'undefined' ? 'Cadastrar novo documento' : 'Editar documento'}</h1>
+        <p>Compartilhe um arquivo com os usuários do sistema</p>
+        <form>
+
+            <FormInput
+                type='text'
+                name='Nome do arquivo'
+                validation='required|min:3'
+                defaultValue={nome_arquivo}
+                setValue={setNomeArquivo}
+                trigger={stepTrigered}
+            />
+
+            {!id && <FormInput
+                type='dropzone'
+                name='Arquivo'
+                validation='required'
+                defaultValue={arquivos}
+                setValue={setArquivos}
+                trigger={stepTrigered}
+                noLabel
+            />}
+
+
+            <FormInput
+                type='date'
+                name='Data de expiração'
+                optional
+                showOptional
+                min={today}
+                tooltip='A partir desta data o documento não será mais exibido para os proprietários'
+                defaultValue={data_expiracao}
+                setValue={setDataExpiracao}
+                trigger={stepTrigered}
+            />
+
+
+            <FormInput
+                type='checkbox'
+                name='Documento disponível para todos os usuários'
+                tooltip='Caso não esteja marcado, apenas usuários com permissão de administrador poderão ver este documento'
+                optional
+                showOptional
+                defaultValue={publico}
+                setValue={setPublico}
+                trigger={stepTrigered}
+            />
+
+
+            <div className='form-controls'>
+                <button className='btn-secondary' onClick={() => history.push('/documentos')}>Cancelar</button>
+                <button className='btn-primary' onClick={submit}>Concluir</button>
+            </div>
+        </form>
+    </div>
+}
